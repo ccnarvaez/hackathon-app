1b22185f47cfa85ebcf3e95093ecc528d68c1261
ccnarvaez <ccnarvaez90@gmail.com>
2021-07-22 18:42:04 -0400

    agregar admin

--- src/components/SidebarAdm.jsx
+++ src/components/SidebarAdm.jsx
@@ -8,7 +8,7 @@ const SidebarAdm = () => {
         <h3 className="menu-options"> Menu </h3>
             <ul className=" side-list">
                 <li> <a href={`/dashboard/${usermail}`}> Dashboard </a> </li>
-                <li> <a href='/userprofile/admins@everis.cl'> Perfil de usuario </a> </li>
+                <li> <a href='/userprofile/admins@everis.cl'> Perfil  </a> </li>
                 <li> <a href={`/search/${usermail}`}> Nuestros colaboradores </a> </li>
                 <li> <a href={`/newadmin/${usermail}`}> Añadir nuevo administrador </a> </li>
             </ul>
